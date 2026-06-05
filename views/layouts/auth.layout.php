37483676fa14fc7a291e3117b4df26fcb9f6a000
nexuusdev <mamefat2004@gmail.com>
2026-06-05 17:41:04 +0200

    partials  et layout

---
+++ views/layouts/auth.layout.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Connexion — Gestion Blogs</title>
+  <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="bg-gradient-to-br from-indigo-100 to-blue-50 font-sans antialiased min-h-screen flex items-center justify-center p-4">
+
+   <?= $content ?>
+
+
+</body>
+</html>
