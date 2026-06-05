97cd5dc8ef88a07863d0ab2259c7dc7c19f4bfc7
nexuusdev <mamefat2004@gmail.com>
2026-06-05 20:45:27 +0200

    Fix: Ajout du slash de fin pour ROOT

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 $protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http';
 define("WEBROOT", $protocol . "://" . $_SERVER['HTTP_HOST'] . "/");
-define("ROOT", str_replace("public","",$_SERVER['DOCUMENT_ROOT']));
+define("ROOT", str_replace("public", "", $_SERVER['DOCUMENT_ROOT']) . "/");
 if(session_status() == PHP_SESSION_NONE){session_start();}
 
 require_once ROOT."config/helpers.php";
