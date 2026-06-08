fcdb06f9d09c44a6fe5f672ad2e0112ca21f951d
nexuusdev <mamefat2004@gmail.com>
2026-06-08 15:48:54 +0200

    Fix: Routage automatique de env.php et env.dev.php

--- public/index.php
+++ public/index.php
@@ -5,6 +5,13 @@ if(session_status() == PHP_SESSION_NONE){session_start();}
 
 require_once ROOT."config/helpers.php";
 require_once ROOT."config/validators.php";
+//si le fichier env.php existe(sur alwaysData)
+if(file_exists(ROOT."env.php")){
+    require_once ROOT . "env.php";
+
+}
+//si je suis en local
+require_once ROOT."env.dev.php";
 // require_once ROOT."view/partials/header.php";
 
 require_once("../routes/web/router.php");
